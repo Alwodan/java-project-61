1a687bda90aa601b3fbf33b629a62faf798ba01a
Vladislav <mistergeralt@gmail.com>
2023-05-25 17:55:33 +0300

    bugfix in Prime game

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -4,15 +4,17 @@ import java.util.Random;
 
 public class Prime {
     private static int currentQuestion;
+    private static final int MAX_NUMBER = 100;
     public static final String MESSAGE = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
     public static String generateQuestion() {
         Random random = new Random();
-        currentQuestion = random.nextInt(100);
+        currentQuestion = random.nextInt(MAX_NUMBER);
         return String.valueOf(currentQuestion);
     }
 
     public static String generateAnswer() {
-        for (int i = 2; i < currentQuestion / 2; i++) {
+        // +1 so number 4 can pass
+        for (int i = 2; i < (currentQuestion / 2) + 1; i++) {
             if (currentQuestion % i == 0) {
                 return "no";
             }
